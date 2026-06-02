2ca600f57791fc53a2a4bd49a0af66098f62e547
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-06-03 00:37:14 +0530

    [Nandhakumar]UC3_GreetingMessage

--- src/main/java/com/bridgelabz/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/controller/GreetingController.java
@@ -2,17 +2,20 @@ package com.bridgelabz.controller;
 
 import com.bridgelabz.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
     @Autowired
     private GreetingService greetingService;
 
-    @GetMapping("/greeting")
-    public String greeting() {
-        return greetingService.getGreetingMessage();
+    @GetMapping
+    public String greeting(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName) {
+
+        return greetingService.getGreetingMessage(firstName,lastName);
     }
 }
--- src/main/java/com/bridgelabz/service/GreetingService.java
+++ src/main/java/com/bridgelabz/service/GreetingService.java
@@ -8,4 +8,21 @@ public class GreetingService {
     public String getGreetingMessage() {
         return "Hello World";
     }
+
+    public String getGreetingMessage(String firstName, String lastName) {
+
+        if(firstName != null && lastName != null) {
+            return "Hello " + firstName + " " + lastName;
+        }
+
+        if(firstName != null) {
+            return "Hello " + firstName;
+        }
+
+        if(lastName != null) {
+            return "Hello " + lastName;
+        }
+
+        return "Hello World";
+    }
 }
