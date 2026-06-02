864f3747daf38167aa7d5bda1f232a6fe0b6073b
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-06-03 00:57:22 +0530

    [Nandhakumar]UC5_SaveGreeting

--- src/main/java/com/bridgelabz/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.controller;
 
+import com.bridgelabz.model.Greeting;
 import com.bridgelabz.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,14 @@ public class GreetingController {
     @Autowired
     private GreetingService greetingService;
 
-    @GetMapping
-    public String greeting(
+    @PostMapping
+    public Greeting saveGreeting(
             @RequestParam(required = false) String firstName,
             @RequestParam(required = false) String lastName) {
 
-        return greetingService.getGreetingMessage(firstName,lastName);
+        String message =
+                greetingService.getGreetingMessage(firstName,lastName);
+
+        return greetingService.saveGreeting(message);
     }
 }
--- src/main/java/com/bridgelabz/service/GreetingService.java
+++ src/main/java/com/bridgelabz/service/GreetingService.java
@@ -1,27 +1,35 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.model.Greeting;
+import com.bridgelabz.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
 
-    public String getGreetingMessage() {
-        return "Hello World";
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    private long id = 1;
+
+    public Greeting saveGreeting(String message) {
+
+        Greeting greeting = new Greeting(id++, message);
+
+        return greetingRepository.save(greeting);
     }
 
     public String getGreetingMessage(String firstName, String lastName) {
 
-        if(firstName != null && lastName != null) {
+        if(firstName != null && lastName != null)
             return "Hello " + firstName + " " + lastName;
-        }
 
-        if(firstName != null) {
+        if(firstName != null)
             return "Hello " + firstName;
-        }
 
-        if(lastName != null) {
+        if(lastName != null)
             return "Hello " + lastName;
-        }
 
         return "Hello World";
     }
